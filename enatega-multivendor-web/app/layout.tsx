863f4cef717e95adfa867eb8e2f4cac89090f1f1
Abdullah <abdullahzahid903126@gmail.com>
2026-07-14 12:51:00 +0500

    Fixed initial web redirection flow.

--- enatega-multivendor-web/app/layout.tsx
+++ enatega-multivendor-web/app/layout.tsx
@@ -7,7 +7,7 @@ import { getLocale, getMessages } from "next-intl/server";
 import Script from "next/script";
 
 export const metadata = {
-  title: "Enatega Multivendor",
+  title: "WALIM Multivendor",
   manifest: "/manifest.json",
 };
 
